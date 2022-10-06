d8ce0146ffea7a7a24b5dcd7924ccd81f429f162
huasmc <huasmc@gmail.com>
2022-10-06 17:55:50 -0400

    Finished Gatsby Tutorial Part 4

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+import Seo from "../components/seo";
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Cooper" src="../images/cooper.jpg" />
+    </Layout>
+  );
+};
+
+export const Head = () => <Seo title="Home Page" />;
+
+export default IndexPage;
